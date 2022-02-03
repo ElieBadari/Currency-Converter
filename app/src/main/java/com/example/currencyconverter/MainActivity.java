247bfd69a36679ed0e279576265033e2ae8a23cb
ElieBadari <elie.badari@hotmail.com>
2022-02-04 00:15:56 +0200

    added spinners and texts (visual only)

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,19 +3,26 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText money_amount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        money_amount = (EditText)findViewById(R.id.money);//links the variable money_amount to the element Text
     }
 
     public void convert(View v){
-        Toast toast = Toast.makeText(getApplicationContext(),"insert conversion here", Toast.LENGTH_LONG);
+        Double input_money =  Double.parseDouble(money_amount.getText().toString());
+
+
+        Toast.makeText(getApplicationContext(),"insert converted amount here", Toast.LENGTH_LONG).show();
 
     }
 }
