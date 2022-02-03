c1303edd351dfd498db49a7d06849d6d79b2fd75
ElieBadari <elie.badari@hotmail.com>
2022-02-04 01:20:50 +0200

    set up the spinner (code)

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -4,31 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText money_amount;
-    Spinner sp1;
-    Button b1;
+    Spinner spinner;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         money_amount = (EditText)findViewById(R.id.money);//links the variable money_amount to the element Text
-        sp1 = (Spinner) findViewById(R.id.currency_sp);
-        b1 = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
+
+        spinner = (Spinner) findViewById(R.id.currency_sp);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.currencies, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
     }
 
     public void convert(View v){
+
         Double input_money =  Double.parseDouble(money_amount.getText().toString());
 
 
         Toast.makeText(getApplicationContext(),"insert converted amount here", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
