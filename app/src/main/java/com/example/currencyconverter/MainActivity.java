898d535bcc7fa9bd49179797fd036e5434c9260c
ElieBadari <elie.badari@hotmail.com>
2022-02-04 00:47:20 +0200

    moddified spinners and text (visual only) and instantiated and linked var to element

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,19 +3,25 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText money_amount;
+    Spinner sp1;
+    Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         money_amount = (EditText)findViewById(R.id.money);//links the variable money_amount to the element Text
+        sp1 = (Spinner) findViewById(R.id.currency_sp);
+        b1 = (Button) findViewById(R.id.button);
     }
 
     public void convert(View v){
