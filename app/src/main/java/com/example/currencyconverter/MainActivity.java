de8015951fe11477b8d9a5463db42922443d1cb7
ElieBadari <elie.badari@hotmail.com>
2022-02-04 03:06:15 +0200

    changed spinner implementation + output formating

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     EditText money_amount;
     Spinner spinner;
@@ -22,44 +22,37 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        money_amount = (EditText)findViewById(R.id.money);//links the variable money_amount to the element Text
+        money_amount = (EditText) findViewById(R.id.money);//links the variable money_amount to the element Text
         button = (Button) findViewById(R.id.button);
 
         spinner = (Spinner) findViewById(R.id.currency_sp);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.currencies, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        String[] currencies = {"USD","LBP"};
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,currencies);
         spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
 
 
     }
 
     public void convert(View v){
 
-        Double output_money;
+        long output_money;
         Double input_money =  Double.parseDouble(money_amount.getText().toString());
+
         if(spinner.getSelectedItem().toString() == "USD"){
-            output_money = input_money/22000;
-            String message = "The input amount (LBP) in USD is: " + output_money;
+            output_money = Math.round(input_money/22000);
+            String message = "The input amount (LBP) in USD is: $" + output_money;
+            Toast.makeText(getApplicationContext(),message, Toast.LENGTH_LONG).show();
+        }
+        else if (spinner.getSelectedItem().toString() == "LBP"){
+            output_money =  Math.round(input_money*22000);
+            String message = "The input amount (USD) in LBP is: " + output_money;
+            Toast.makeText(getApplicationContext(),message, Toast.LENGTH_LONG).show();
         }
         else{
-            output_money = input_money*22000;
-            String message = "The input amount (USD) in LBP is: " + output_money;
-
+            Toast.makeText(getApplicationContext(),"Select Currency",Toast.LENGTH_LONG).show();
         }
-        Toast.makeText(getApplicationContext(),"message", Toast.LENGTH_LONG).show();
+
 
     }
 
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
 }
