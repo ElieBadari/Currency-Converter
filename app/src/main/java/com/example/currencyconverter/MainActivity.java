9654126a6191bc315dbc6286fa7fbd6151cc0212
ElieBadari <elie.badari@hotmail.com>
2022-02-04 03:37:18 +0200

    Logic and output formatting is working

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText money_amount;
@@ -23,33 +26,44 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         money_amount = (EditText) findViewById(R.id.money);//links the variable money_amount to the element Text
-        button = (Button) findViewById(R.id.button);
 
+        //instantiate spinner and load it with preset currencies
         spinner = (Spinner) findViewById(R.id.currency_sp);
-        String[] currencies = {"USD","LBP"};
+        String[] currencies = {"","USD","LBP"};
         ArrayAdapter adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,currencies);
         spinner.setAdapter(adapter);
 
 
+
+
+
     }
 
-    public void convert(View v){
 
-        long output_money;
+    public void convert(View v){
+        //for string formatting aesthetic purposes, i don't know how it will affect performance
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setGroupingUsed(true);
+        df.setGroupingSize(3);
+
+        Double output_money;
         Double input_money =  Double.parseDouble(money_amount.getText().toString());
+        String formatted_output;
 
         if(spinner.getSelectedItem().toString() == "USD"){
-            output_money = Math.round(input_money/22000);
-            String message = "The input amount (LBP) in USD is: $" + output_money;
+            output_money = input_money/22000.0;
+            formatted_output = df.format(output_money);
+            String message = "The input amount (LBP) in USD is: " + formatted_output + "USD";
             Toast.makeText(getApplicationContext(),message, Toast.LENGTH_LONG).show();
         }
         else if (spinner.getSelectedItem().toString() == "LBP"){
-            output_money =  Math.round(input_money*22000);
-            String message = "The input amount (USD) in LBP is: " + output_money;
+            output_money =  input_money*22000.0;
+            formatted_output = df.format(output_money);
+            String message = "The input amount (USD) in LBP is: " + formatted_output + "LBP";
             Toast.makeText(getApplicationContext(),message, Toast.LENGTH_LONG).show();
         }
         else{
-            Toast.makeText(getApplicationContext(),"Select Currency",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(),"Select Currency",Toast.LENGTH_SHORT).show();
         }
 
 
